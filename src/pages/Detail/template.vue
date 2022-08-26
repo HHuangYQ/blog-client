cfd449fc3ec5b9c6a2602cea56c6eb9aac8110f4
HHuangYQ <1135782479@qq.com>
2022-08-26 13:09:41 +0800

    编写detail页面跟css

--- src/pages/Detail/template.vue
+++ src/pages/Detail/template.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="detail">
-   博客详情
+  <div id="detail">
+   <section class="user-info">
+    <img src="../../assets/ avatar.png" alt="">
+    <h3>前端异步大揭秘</h3>
+    <p><router-link to="/user">某某某</router-link>发布于3天前</p>
+   </section>
   </div>
 </template>
 
@@ -8,7 +12,6 @@
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style src="./template.css">
+<style scoped lang="less" src="./template.less">
 
 </style>
