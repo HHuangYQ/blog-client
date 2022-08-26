e2b09603ccc903abe81409039ac991a4f57595bc
HHuangYQ <1135782479@qq.com>
2022-08-26 13:20:04 +0800

    编写my页面跟css  调整img头像大小

--- src/pages/My/template.vue
+++ src/pages/My/template.vue
@@ -1,6 +1,38 @@
 <template>
-  <div class="my">
-   我的
+  <div id="my">
+   <section class="user-info">
+    <img src="../../assets/ avatar.png" alt="">
+      <h3>某某某</h3>
+   </section>
+   <section>
+    <div class="item">
+      <div class="date">
+        <span class="day">20</span>
+        <span class="month">5月</span>
+        <span class="year">2022</span>
+      </div>
+      <h3>前端异步解密</h3>
+      <p>本文以一个本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，简单的文件读写为例，</p>
+      <div class="actions">
+        <router-link to="/edit">编辑</router-link>
+        <a href="#">删除</a>
+      </div>
+    </div>
+    
+    <div class="item">
+      <div class="date">
+        <span class="day">20</span>
+        <span class="month">5月</span>
+        <span class="year">2022</span>
+      </div>
+      <h3>前端异步解密</h3>
+      <p>本文以一个本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，简单的文件读写为例，</p>
+      <div class="actions">
+        <router-link to="/edit">编辑</router-link>
+        <a href="#">删除</a>
+      </div>
+    </div>
+   </section>
   </div>
 </template>
 
@@ -8,7 +40,6 @@
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style src="./template.css">
+<style scoped  lang="less" src="./template.less">
 
 </style>
