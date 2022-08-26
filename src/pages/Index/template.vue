4911400ce99c2ea8dd15225a0ed5d3686ff9d0ba
HHuangYQ <1135782479@qq.com>
2022-08-26 12:57:31 +0800

    编写index模版和css

--- src/pages/Index/template.vue
+++ src/pages/Index/template.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="index">
-   <p>首页</p>
+  <div id="index">
+   <section class="blog-posts">
+    <div class="item">
+      <figure class="avatar">
+        <img src="../../assets/ avatar.png" alt="">
+        <figcaption>某某某</figcaption>
+      </figure>
+      <h3>前端异步大揭秘<span>3天前</span></h3>
+      <p>本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，</p>
+    </div>
+    <div class="item">
+      <figure class="avatar">
+        <img src="../../assets/ avatar.png" alt="">
+        <figcaption>某某某</figcaption>
+      </figure>
+      <h3>前端异步大揭秘<span>3天前</span></h3>
+      <p>本文以一个本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，本文以一个简单的文件读写为例，简单的文件读写为例，</p>
+    </div>
+   </section>
   </div>
 </template>
 
@@ -8,7 +25,6 @@
 
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less" src="./template.less">
 
 </style>
