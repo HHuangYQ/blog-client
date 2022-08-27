0a898754ab153b931bf5e256efd45bf62c6b8b51
HHuangYQ <1135782479@qq.com>
2022-08-27 19:11:06 +0800

    init

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -52,19 +52,22 @@ export default {
 </script>
 
 <style lang="less">
-@import "../assets/base.less";
+
+@import "../assets/base.less";  
 
 header.no-login {
   padding: 0 12% 30px 12%;
   background: @bgColor;
   display: grid;
   justify-items: center;
+
   h1 {
     color: #fff;
     font-size: 40px;
     margin: 60px 0 0 0;
     text-transform: uppercase;
   }
+
   p {
     margin: 15px 0 0 0;
     color: #fff;
@@ -73,28 +76,35 @@ header.no-login {
   .btns {
     margin-top: 20px;
   }
+
   button {
     margin: 20px 5px 0;
   }
 } 
+
+
 header.login {
   display: flex;
   align-items: center;
   background: @bgColor;
+
   h1 {
     margin: 0;
     padding: 0;
     font-size: 40px;
     text-transform: uppercase;
     flex: 1;
+
     a {
       color: #fff;
     }
   }
+
   .edit {
     color: #fff;
     font-size: 30px;
   }
+
   .avatar {
     width: 40px;
     height: 40px;
@@ -102,8 +112,10 @@ header.login {
     border-radius: 50%;
     margin-left: 15px;
   }
+
   .user {
     position: relative;
+
     ul {
       display: none;
       position: absolute;
@@ -113,20 +125,27 @@ header.login {
       margin:0;
       padding: 0;
       background-color: #fff;
+
       a {
         text-decoration: none;
         color: #333;
         font-size: 12px;
         display: block;
         padding: 5px 10px;
+
         &:hover {
           background-color: #eaeaea;
         }
       }
+
     }
+
     &:hover ul {
       display: block;
     }
   }
 }
+
+
+
 </style>
